7a81788157a9bf6285b9b7e88a5228bcfcd73719
Prahlad Alaparthi <alaparthi.prahlad@gmail.com>
2026-02-04 08:57:51 +0530

    explained

--- load_data.py
+++ load_data.py
@@ -31,26 +31,26 @@ class FRBDataset(Dataset):
         k_dm = 4.1488e3
         f_high = freq_array[-1]
 
-        delays_ms = k_dm * dm * (freq_array**-2 - f_high**-2)
+        delays_ms = k_dm * dm * (freq_array**-2 - f_high**-2) # Dispersion delay equation
         delays_bins = (delays_ms / time_res_ms).astype(int)
 
         dispersed_wfall = np.zeros_like(waterfall)
         for i in range(waterfall.shape[0]):
             shift = delays_bins[i]
             if shift < waterfall.shape[1]:
-                dispersed_wfall[i, :] = np.roll(waterfall[i, :], shift)
+                dispersed_wfall[i, :] = np.roll(waterfall[i, :], shift) # applies dispersion delay by shifting signal
                 if shift > 0:
-                    dispersed_wfall[i, :shift] = np.random.normal(0, 1, shift)
+                    dispersed_wfall[i, :shift] = np.random.normal(0, 1, shift) # fill element if shifted = true
             else:
                 dispersed_wfall[i, :] = np.random.normal(
-                    0, 1, waterfall.shape[1])
+                    0, 1, waterfall.shape[1]) # fills entire frequency channel with noise (signal completely shifted out)
         return dispersed_wfall
 
     def __getitem__(self, idx):
         if self.mode == 'synthetic':
-            dm = np.random.uniform(100, 2000)
+            dm = np.random.uniform(100, 2000) # could try diff values but this range is reasonable
             img = gen_synthetic_frb(dm)
-            return torch.from_numpy(img).float().unsqueeze(0), torch.tensor([dm / 1000.0]).float()
+            return torch.from_numpy(img).float().unsqueeze(0), torch.tensor([dm / 1000.0]).float() # converting to tensor, normalizing dm
 
         try:
             with h5py.File(self.real_files[idx], 'r') as f:
@@ -59,18 +59,20 @@ class FRBDataset(Dataset):
                 img = resize(raw_data, (256, 256), anti_aliasing=True)
 
                 freq_new = np.linspace(800, 400, 256)
-                synthetic_time_res = 1000.0 / 256.0
+                synthetic_time_res = 1000.0 / 256.0 # same roughly 3.6ms per pixel
                 img = self.redisperse_waterfall(
                     img, dm, freq_new, synthetic_time_res)
 
+                ' robust normalization to handle RFI and outliers '
                 median = np.median(img)
                 q75, q25 = np.percentile(img, [75, 25])
-                iqr = q75 - q25
+                iqr = q75 - q25 # interquartile range
                 sigma_robust = iqr / 1.3489
 
                 if sigma_robust == 0:
                     sigma_robust = 1e-6
 
+                ' explained in synthetic_frbs.py '
                 img = np.clip(img, median - 3*sigma_robust,
                               median + 3*sigma_robust)
                 img = (img - median) / sigma_robust
--- synthetic_frbs.py
+++ synthetic_frbs.py
@@ -1,18 +1,26 @@
 import numpy as np
 import scipy.ndimage as ndimage
 
+'''
+Synthetic FRBs data is needed for testing because real FRB data is not always available.
+This module generates synthetic FRB waterfall data with realistic characteristics, including dispersion, noise, and signal profiles.
+We can also say that there is 'shortage of data' bacause real FRB events are rare and limited in number
+'''
 
+'''
+CHIME (leading frb detectors) operates in the frequency range of 400 MHz to 800 MHz.
+'''
 def gen_synthetic_frb(dm, n_freq=256, n_time=256, freq_low=400, freq_high=800):
 
     freqs = np.linspace(freq_high, freq_low, 256)
     k_dm = 4.1488e3  # Dispersion constant in MHz^2 pc^-1 cm^3 ms
 
-    # Calculating dispersion delay of signals in milliseconds
-    delays_ms = k_dm * dm * ((freqs)**-2 - (freq_high)**-2)
-    time_res = 1000.0 / n_time
-    delay_indices = (delays_ms / time_res).astype(int)
+    ' Calculating dispersion delay of signals in milliseconds '
+    delays_ms = k_dm * dm * ((freqs)**-2 - (freq_high)**-2) # Dispersion delay equation used for radio waves travelling through ISM
+    time_res = 1000.0 / n_time # each pixel represents roughly 3.9 ms 
+    delay_indices = (delays_ms / time_res).astype(int) # converting delay from ms to array indices
 
-    # Adding random noise and data to match realistic FRB characteristics
+    ' Adding random noise and data to match realistic FRB characteristics '
     waterfall = np.random.normal(0, 2.0, (n_freq, n_time))
 
     channel_gains = np.random.normal(0, 5.0, (n_freq, 1))
@@ -20,17 +28,18 @@ def gen_synthetic_frb(dm, n_freq=256, n_time=256, freq_low=400, freq_high=800):
 
     blobs = ndimage.gaussian_filter(
         np.random.normal(0, 2.0, (n_freq, n_time)), sigma=2.0)
-    waterfall += blobs
+    waterfall += blobs # stimulates RFI and atmospheric effects
 
-    center_time_bin = np.random.randint(int(n_time * 0.15), int(n_time * 0.7))
+    ' Randomize even these features so they dont overfit to a specific pattern '
+    center_time_bin = np.random.randint(int(n_time * 0.15), int(n_time * 0.7)) # ensures signal is visible and not cutoff at edges
     pulse_width = np.random.randint(10, 40)
 
-    # Creating the profile of FRB signal
+    ' Creating the profile of FRB signal '
     for i in range(n_freq):
         t_start = center_time_bin + delay_indices[i]
         if t_start + pulse_width < n_time:
             x = np.linspace(-2, 2, pulse_width)
-            profile = np.exp(-x**2)
+            profile = np.exp(-x**2) # bell curve for realistic appearance
 
             signal_strength = np.random.uniform(5, 12)
             waterfall[i, t_start: t_start +
@@ -38,9 +47,16 @@ def gen_synthetic_frb(dm, n_freq=256, n_time=256, freq_low=400, freq_high=800):
 
     waterfall = ndimage.gaussian_filter(waterfall, sigma=(0.5, 1.0))
 
+    '''
+    In radio astronomy, a single massive interference spike can ruin the scale of the whole image.
+    This step ensures that the extreme outliers dont wash out the rest of the image features, 
+    keeping the background texture visible.
+    '''
+    
+    ' Calculates std and trims the image to remove extreme outliers '
     std_val = np.std(waterfall)
     waterfall = np.clip(waterfall, -3*std_val, 3*std_val)
 
-    waterfall = (waterfall - np.mean(waterfall)) / (np.std(waterfall) + 1e-6)
+    waterfall = (waterfall - np.mean(waterfall)) / (np.std(waterfall) + 1e-6) # basic z-score normalization
 
     return waterfall
